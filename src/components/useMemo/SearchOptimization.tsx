f13d073e97bcb8d202437a805c2fe4eaea935d57
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-01-26 12:57:08 +0900

    useMemo와 useState를 활용한 검색연습

---
+++ src/components/useMemo/SearchOptimization.tsx
@@ -0,0 +1,51 @@
+import React, { useMemo, useRef, useState } from 'react';
+
+const SearchOptimization: React.FC = () => {
+  const [items, setItems] = useState<string[]>([]);
+  const [query, setQuery] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null) as any;
+  //useMemo를 활용해 이렇게 검색결과에 대한 최적화를 해줄 수도 있다. state배열을 두개나 정의할 필요가 없으며 리렌더링에 대한 걱정도 줄일 수 있다!
+  const filteredItems = useMemo(() => {
+    return items.filter(item => {
+      return item.toLowerCase().includes(query.toLowerCase())
+    })
+  }, [items, query]);
+
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const value = inputRef.current.value;
+    if(value === "") return;
+    setItems(item => {
+      return [...item, value];
+    });
+    inputRef.current.value = "";
+  };
+
+  return (
+    <>
+      Search: 
+      <input
+        value={query}
+        onChange={e => setQuery(e.target.value)}></input>
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type="text"></input>
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items</h3>
+      {
+        filteredItems.map(item => {
+          <div>
+            {
+              item
+            }
+          </div>
+        })
+      }
+    </>
+  );
+};
+
+export default SearchOptimization;
---
+++ src/components/useState/DirtySearch.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from 'react';
+
+const DirtySearch: React.FC = () => {
+  //검색을 한 결과를 보여주기위해서 useState배열을 두개나 사용한다? 이게 맞을까?
+  const [items, setItems] = useState<string[]>([]);
+  const [filteredItems, setFilteredItems] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null) as any;
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const value = inputRef.current.value;
+    if(value === "") return;
+    setItems(item => {
+      return [...item, value];
+    });
+    setFilteredItems(item => {
+      return [...item, value];
+    });
+    inputRef.current.value = "";
+  }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setFilteredItems(
+      items.filter(item => {item.toLowerCase().includes(value.toLowerCase())})
+    );
+  }
+  return (
+    <>
+      Search: 
+      <input
+        onChange={onChange}></input>
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        New Item: <input ref={inputRef} type="text"></input>
+        <button type="submit">Add</button>
+      </form>
+      <h3>Items</h3>
+      {
+        filteredItems.map(item => {
+          <div>
+            {
+              item
+            }
+          </div>
+        })
+      }
+    </>
+  );
+};
+
+export default DirtySearch;
