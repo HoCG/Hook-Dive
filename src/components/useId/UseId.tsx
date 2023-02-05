63febd8ff96acf94fa88e8eba9f8341947eea374
HoCG <rhwkd5012@naver.com>
2023-02-05 22:23:20 +0900

    설명 추가

--- src/components/useId/UseId.tsx
+++ src/components/useId/UseId.tsx
@@ -8,6 +8,8 @@ type user = {
 };
 
 const UseId = () => {
+  //useId를 통해 고유의 값을 생성시킬 수 있다. 이를 기반으로 key를 지정할때에 피로를 덜어낼 수 있으며 좀 더 백엔드 친화적인
+  //Id값을 생성시킬 수 있다는 장점이 있다!
   const [userInfo, setUserInfo] = useState<user[]>([
     {
       id: useId(),
