96430be7424b28cb0bf8c853678aca3ed6e0e58f
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-02-06 16:11:17 +0900

    feat: useInfiniteQuery. 언젠간 꼭 해보자.

---
+++ src/components/useInfiniteQuery/UseInfiniteQuery.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+const UseInfiniteQuery = () => {
+  //대용량 데이터가 있어야 확인하는 맛이 좀 나는 useInfiniteQuery... 지금은 가지고있는 데이터의 양이 많이없어서 사용에 제약이 걸린다. 나중에 기회가 된다면 이 훅을 활용해서
+  //재밌는 화면을 만들어보도록 하자.
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default UseInfiniteQuery;
